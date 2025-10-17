b510f8a97d913b675de210c6d820c1bbd77085f6
Simon Griman <rayoysombra@gmail.com>
2025-10-17 12:11:42 -0400

    Añadir notificacion de registro creado o actualizado

--- resources/views/consolidado/index.blade.php
+++ resources/views/consolidado/index.blade.php
@@ -9,6 +9,32 @@
 @section('content')
     @livewireStyles
 
+    @if (session('crear'))
+        @section('js')
+            <script src="{{ url('js/sweetalert2.js') }}"></script>
+            <script>
+                Swal.fire(
+                    "¡Hecho!",
+                    "{{ session('crear') }}",
+                    "success"
+                )
+            </script>
+        @stop
+    @endif
+
+    @if (session('actualizar'))
+        @section('js')
+            <script src="{{ url('js/sweetalert2.js') }}"></script>
+            <script>
+                Swal.fire(
+                    "¡Hecho!",
+                    "{{ session('actualizar') }}",
+                    "success"
+                )
+            </script>
+        @stop
+    @endif
+
     {{-- <img src="{{ url('storage/' . $cintillo) }}" alt="" class="cintillo" style="width:100%"> --}}
     
     
