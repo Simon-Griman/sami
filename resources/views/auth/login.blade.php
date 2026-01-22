43937c781f84544344f2e8c678547f6d1fee9c86
Simon Griman <rayoysombra@gmail.com>
2026-01-22 13:17:44 -0400

    logo sami

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,9 @@
 <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
-            <x-jet-authentication-card-logo />
+            <div class="text-center">
+                <img src="{{ url('vendor/adminlte/dist/img/sami.png') }}" alt="" width="100">
+            </div>
         </x-slot>
 
         <div class="card-body">
